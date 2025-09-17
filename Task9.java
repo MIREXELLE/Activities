2f22a8d185d1c8cd073a2097849a1ba8942b96e1
Mirexelle <m.mirexellevincentc.piol@gmail.com>
2025-09-17 16:50:33 +0800

    task 9

--- Task9.java
+++ Task9.java
@@ -1,2 +1,44 @@
-public class Task9 {
-}
+
+import static java.lang.Math.multiplyExact;
+import static java.lang.Math.floorDiv;
+import static java.lang.Math.addExact;
+import static java.lang.Math.subtractExact;
+
+import java.util.Scanner;
+
+public class Task9{
+
+        public static int add(int a, int b) {
+            return addExact(a, b) ;
+        }
+
+        public static int subtract(int a, int b) {
+            return subtractExact(a, b) ;
+        }
+
+        public static int multiply(int a, int b) {
+            return multiplyExact(a, b);
+        }
+
+        public static float divide(int a, int b) {
+            if(b == 0) {
+                System.out.println("Division: Cannot divide by zero");
+            }
+
+            return floorDiv(a, b);
+        }
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter first number: ");
+            int a = scan.nextInt();
+            System.out.print("Enter second number: ");
+            int b = scan.nextInt();
+            System.out.println("Addition: " + add(a, b));
+            System.out.println("Substraction: " + subtract(a, b));
+            System.out.println("Multiplication: " + multiply(a, b));
+            System.out.println("Division: " + divide(a, b));
+
+        }
+    }
+

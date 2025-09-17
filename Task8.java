744f5a40c07648912a50c4c0a0f7737362c48511
Mirexelle <m.mirexellevincentc.piol@gmail.com>
2025-09-17 16:05:11 +0800

    task 8

--- Task8.java
+++ Task8.java
@@ -13,23 +13,24 @@ public class Task8 {
         System.out.print("Enter third number: ");
         num3 = scan.nextInt();
 
-        int comulative1 = 0;
-        int comulative2 = 0;
-        int comulative3 = 0;
+        int cumulative1 = 0;
+        int cumulative2 = 0;
+        int cumulative3 = 0;
+
         for (int i = 0 ; i <= num1; i++) {
-            comulative1 += i;
+            cumulative1 += i;
         }
         for (int i = 0 ; i <= num2; i++) {
-            comulative2 += i;
+            cumulative2 += i;
         }
         for (int i = 0 ; i <= num3; i++) {
-            comulative3 += i;
+            cumulative3 += i;
         }
-        int sum =  comulative1 + comulative2 + comulative3;
+        int sum =  cumulative1 + cumulative2 + cumulative3;
 
-        System.out.println("Comulative sum of the first number: " + comulative1);
-        System.out.println("Comulative sum of the second number: " + comulative2);
-        System.out.println("Comulative sum of the third number: " + comulative3);
+        System.out.println("Cumulative sum of the first number: " + cumulative1);
+        System.out.println("Cumulative sum of the second number: " + cumulative2);
+        System.out.println("Cumulative sum of the third number: " + cumulative3);
         System.out.println("Sum of all: " + sum );
     }
 }

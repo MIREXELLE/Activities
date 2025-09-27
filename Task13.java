f02b47377e2c923b4fe62f05e317eb3b7f5767db
Mirexelle <m.mirexellevincentc.piol@gmail.com>
2025-09-27 20:34:09 +0800

    task13

--- Task13.java
+++ Task13.java
@@ -1,25 +1,24 @@
-public class Task13 {
 
-     static class vehicle{
-        protected String make;
-        protected String model;
-        protected int year;
+    class vehicle{
+         String make;
+         String model;
+         int year;
 
-        public vehicle(String make, String model, int year){
+         vehicle(String make, String model, int year){
             this.make = make;
             this.model = model;
             this.year = year;
         }
     }
-    static class Car extends vehicle{
+    class Car extends vehicle{
          int numberOfDoors;
 
-         public Car(String make, String model, int year, int numberOfDoors){
+         Car(String make, String model, int year, int numberOfDoors){
              super (make, model, year);
              this.numberOfDoors = numberOfDoors;
          }
 
-        public void displayDetails(){
+         void displayDetails(){
             System.out.println("Car Details: ");
             System.out.println("Make: " + this.make);
             System.out.println("Model: " + this.model);
@@ -27,11 +26,9 @@ public class Task13 {
             System.out.println("Number of Doors: " + this.numberOfDoors);
         }
     }
-
-    public static void main(String[] args) {
-
-        Car mycar = new Car("Mustang","Lamborgini", 2001,  4);
-        mycar.displayDetails();
+    public class Task13 {
+        public static void main(String[] args) {
+            Car mycar = new Car("Mustang", "Lamborgini", 2001, 4);
+            mycar.displayDetails();
+        }
     }
-
-}
